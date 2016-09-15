a4d1d4f08bf2dc5d6ab26b9e637c7ec5005135cc
Kaya <kayalion@kayalion.net>
2016-09-15 12:43:00 +0200

    use sleep from system in worker script

--- src/worker.sh
+++ src/worker.sh
@@ -4,11 +4,12 @@ DIRECTORY="<your-ride-installation>"
 CONSOLE="$DIRECTORY/application/cli.php"
 PHP="php"
 QUEUE="default"
-SLEEP_TIME=5
+SLEEP_TIME=10
 LOG_FILE="$DIRECTORY/application/data/log/worker.log"
 PRIORITY=5
 
 while true
 do
-	nice -n 5 $PHP $CONSOLE queue worker $QUEUE $SLEEP_TIME >> $LOG_FILE 2>> $LOG_FILE
+    nice -n 5 $PHP $CONSOLE queue worker $QUEUE >> $LOG_FILE 2>> $LOG_FILE
+    sleep $SLEEP_TIME
 done
